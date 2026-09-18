9036622a366877bb5a06043d28236d52885ea44f
Lucky Acer <info@jstdo.uk>
2026-09-19 00:28:42 +0200

    AutoCommit_2026-09-19_00-28-42

--- config/redisClient.js
+++ config/redisClient.js
@@ -1,8 +1,7 @@
 import { createClient } from 'redis'; // Note: 'ioredis' has different API; if you use 'redis' npm, this is correct
 import { redisHost, redisPort } from "../keys.js";
 import TimeUtils from '../utils/Time.js';
-const { formattedDate, getShortTime, getMidTime } = TimeUtils;
-
+const { formattedDate, getShortTime, getMidTime, getLongTime } = TimeUtils;
 
 const redisClient = new createClient({
   socket: {
